39f74809832736bc83e2bddf73937bb67eb80129
Lucas Victor <128725402+lucasvictorm@users.noreply.github.com>
2024-03-04 13:04:34 -0300

    estilo da página de login

--- login.php
+++ login.php
@@ -4,16 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
-    <form action="authentication.php" method="post">
+    <main>
 
-        <label for="login">Login</label>
-        <input type="number" maxlength="5" id="login" name="login">
-        <br>
-        <label for="senha">Senha</label>
-        <input type="password" name="senha" id="senha">
-        <input type="submit" value="Entrar">
-    </form>
+        <img src="./imagens/sirius-logo.png" alt="Logo Sirius">
+        <form action="authentication.php" method="post">
+            <div>
+                <label for="login">Login</label>
+                <input type="number" maxlength="5" id="login" name="login">
+
+            </div>
+            
+            <div>
+                <label for="senha">Senha</label>
+                <input type="password" name="senha" id="senha">
+                
+            </div>
+            <div>
+            <input type="submit" value="Entrar">
+            </div>
+            
+            
+        </form>
+    </main>
 </body>
 </html>
