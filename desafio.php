6b1ef9b8c9b13c9b5cf03aedbebae83ea1f2f37a
Lucas Victor <128725402+lucasvictorm@users.noreply.github.com>
2024-03-04 13:29:51 -0300

    atualização de estilo

--- desafio.php
+++ desafio.php
@@ -10,22 +10,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio</title>
+    <link rel="stylesheet" href="desafio.css">
 </head>
 <body>
-    <h1><?=$sql['nome']?></h1>
-    <form action="concluir.php" enctype="multipart/form-data" method="post" >
-        <input type="hidden" name="desafio_id" value=<?=$desafio_id?>>
-        <label for="foto">Enviar foto</label>
-        <br>
-        <input type="file" name="foto" id="foto">
-        <br>
-        <label for="relatorio">Relatório</label>
-        <textarea name="relatorio" id="relatorio" cols="60" rows="10"></textarea>
+    <?php include "./header.php"?>
+    <main>
+        <h1><?=$sql['nome']?></h1>
+        <form action="concluir.php" enctype="multipart/form-data" method="post" >
+            <input type="hidden" name="desafio_id" value=<?=$desafio_id?>>
+            <div>
+                <label for="foto">Enviar foto</label>
+                
+                <input type="file" name="foto" id="foto">
+            </div>
+            
+            <div>
+                <label for="relatorio">Relatório</label>
+                <textarea name="relatorio" id="relatorio" cols="60" rows="15"></textarea>
+            </div>
+        
 
-        <input type="submit" value="Enviar">
+            <input type="submit" value="Enviar">
 
-    </form>
+        </form>
 
-    
+    </main>
 </body>
 </html>
